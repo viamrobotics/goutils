91b5609929e5b4213f518654da5ef23cf590b481
Eric Daniels <eric@erdaniels.com>
2021-10-20 16:21:47 -0400

    Pass webrtc.Configuration through webrtc.NewPeerConnection

--- rpc/webrtc/peer.go
+++ rpc/webrtc/peer.go
@@ -19,7 +19,7 @@ var DefaultWebRTCConfiguration = webrtc.Configuration{
 }
 
 func newPeerConnectionForClient(ctx context.Context, config webrtc.Configuration, logger golog.Logger) (pc *webrtc.PeerConnection, dc *webrtc.DataChannel, err error) {
-	pc, err = webrtc.NewPeerConnection(DefaultWebRTCConfiguration)
+	pc, err = webrtc.NewPeerConnection(config)
 	if err != nil {
 		return nil, nil, err
 	}
