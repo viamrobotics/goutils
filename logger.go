000e2a153421d7230b35eb346804ddfdd2374911
Eric Daniels <eric@erdaniels.com>
2021-10-21 21:12:07 -0400

    add logger

---
+++ logger.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"github.com/edaniels/golog"
+)
+
+// Logger is used various parts of the package for informational/debugging purposes.
+var Logger = golog.Global
+
+// Debug is helpful to turn on when the library isn't working quite right.
+var Debug = false
