7c7bb8787addfb1039c6da3a4b2d26931f3e678a
Eric Daniels <eric@erdaniels.com>
2021-12-23 18:07:15 -0500

    fix TestResolveFile

--- internal/file_test.go
+++ internal/file_test.go
@@ -14,5 +14,5 @@ func TestResolveFile(t *testing.T) {
 	resolved := ResolveFile("internal/file_test.go")
 	rd, err := ioutil.ReadFile(resolved)
 	test.That(t, err, test.ShouldBeNil)
-	test.That(t, bytes.Contains(rd, []byte("var sentinel = \"great\"")), test.ShouldBeTrue)
+	test.That(t, bytes.Contains(rd, []byte("sentinel := \"great\"")), test.ShouldBeTrue)
 }
