a3f5deb22b4f7853bbbe2f0e5f26b49983c70e3e
Cheuk Tse <cheuk@viam.com>
2022-09-19 23:37:44 -0400

    add server tests

--- rpc/wrtc_server_channel_test.go
+++ rpc/wrtc_server_channel_test.go
@@ -248,3 +248,201 @@ func TestWebRTCServerChannel(t *testing.T) {
 
 	<-messagesRead
 }
+
+func TestWebRTCServerChannelResetStream(t *testing.T) {
+	testutils.SkipUnlessInternet(t)
+	logger := golog.NewTestLogger(t)
+	pc1, pc2, dc1, dc2 := setupWebRTCPeers(t)
+
+	clientCh := newWebRTCClientChannel(pc1, dc1, logger)
+	defer func() {
+		test.That(t, clientCh.Close(), test.ShouldBeNil)
+	}()
+
+	server := newWebRTCServer(logger)
+	// use signaling server just as some random service to test against.
+	// It helps that it is in our package.
+	queue := newMemoryWebRTCCallQueueTest()
+	defer queue.Close()
+	signalServer := NewWebRTCSignalingServer(queue, nil)
+	server.RegisterService(
+		&webrtcpb.SignalingService_ServiceDesc,
+		signalServer,
+	)
+
+	serverCh := newWebRTCServerChannel(server, pc2, dc2, logger)
+	defer func() {
+		test.That(t, serverCh.Close(), test.ShouldBeNil)
+	}()
+
+	<-clientCh.Ready()
+	<-serverCh.Ready()
+
+	var expectedMessagesMu sync.Mutex
+	var expectedMessages []*webrtcpb.Response
+	var messagesRead chan struct{}
+	clientCh.dataChannel.OnMessage(func(msg webrtc.DataChannelMessage) {
+		expectedMessagesMu.Lock()
+		defer expectedMessagesMu.Unlock()
+		req := &webrtcpb.Response{}
+		test.That(t, proto.Unmarshal(msg.Data, req), test.ShouldBeNil)
+		logger.Debugw("got message", "actual", req)
+		test.That(t, expectedMessages, test.ShouldNotBeEmpty)
+		expected := expectedMessages[0]
+		test.That(t, proto.Equal(expected, req), test.ShouldBeTrue)
+		expectedMessages = expectedMessages[1:]
+		if len(expectedMessages) == 0 {
+			close(messagesRead)
+		}
+	})
+	t.Run("reset stream before message sent", func(t *testing.T) {
+		expectedMessagesMu.Lock()
+		expectedMessages = []*webrtcpb.Response{
+			{
+				Stream: &webrtcpb.Stream{Id: 1},
+				Type: &webrtcpb.Response_Headers{
+					Headers: &webrtcpb.ResponseHeaders{},
+				},
+			},
+			{
+				Stream: &webrtcpb.Stream{Id: 1},
+				Type: &webrtcpb.Response_Trailers{
+					Trailers: &webrtcpb.ResponseTrailers{
+						Status: ErrorToStatus(status.Error(codes.Canceled, "request cancelled")).Proto(),
+					},
+				},
+			},
+		}
+		messagesRead = make(chan struct{})
+		expectedMessagesMu.Unlock()
+
+		test.That(t, clientCh.writeHeaders(&webrtcpb.Stream{
+			Id: 1,
+		}, &webrtcpb.RequestHeaders{
+			Method: "/proto.rpc.webrtc.v1.SignalingService/Call",
+			Metadata: metadataToProto(metadata.MD{
+				"rpc-host": []string{"yeehaw"},
+			}),
+		}), test.ShouldBeNil)
+		test.That(t, clientCh.writeReset(&webrtcpb.Stream{Id: 1}), test.ShouldBeNil)
+		<-messagesRead
+	})
+	t.Run("reset stream in middle of message", func(t *testing.T) {
+		expectedMessagesMu.Lock()
+		expectedMessages = []*webrtcpb.Response{
+			{
+				Stream: &webrtcpb.Stream{Id: 1},
+				Type: &webrtcpb.Response_Headers{
+					Headers: &webrtcpb.ResponseHeaders{},
+				},
+			},
+			{
+				Stream: &webrtcpb.Stream{Id: 1},
+				Type: &webrtcpb.Response_Trailers{
+					Trailers: &webrtcpb.ResponseTrailers{
+						Status: ErrorToStatus(status.Error(codes.Canceled, "request cancelled")).Proto(),
+					},
+				},
+			},
+		}
+		messagesRead = make(chan struct{})
+		expectedMessagesMu.Unlock()
+
+		test.That(t, clientCh.writeHeaders(&webrtcpb.Stream{
+			Id: 1,
+		}, &webrtcpb.RequestHeaders{
+			Method: "/proto.rpc.webrtc.v1.SignalingService/Call",
+			Metadata: metadataToProto(metadata.MD{
+				"rpc-host": []string{"yeehaw"},
+			}),
+		}), test.ShouldBeNil)
+
+		reqMd, err := proto.Marshal(&webrtcpb.CallRequest{Sdp: "hello"})
+		test.That(t, err, test.ShouldBeNil)
+
+		test.That(t, clientCh.writeMessage(&webrtcpb.Stream{
+			Id: 1,
+		}, &webrtcpb.RequestMessage{
+			HasMessage: true,
+			PacketMessage: &webrtcpb.PacketMessage{
+				Data: reqMd,
+				Eom:  false,
+			},
+			Eos: false,
+		}), test.ShouldBeNil)
+		test.That(t, clientCh.writeReset(&webrtcpb.Stream{Id: 1}), test.ShouldBeNil)
+		<-messagesRead
+	})
+	t.Run("reset stream after message", func(t *testing.T) {
+		expectedMessagesMu.Lock()
+		respMd, err := proto.Marshal(&webrtcpb.CallResponse{
+			Uuid: "insecure-uuid-1",
+			Stage: &webrtcpb.CallResponse_Init{
+				Init: &webrtcpb.CallResponseInitStage{
+					Sdp: "world",
+				},
+			},
+		})
+		test.That(t, err, test.ShouldBeNil)
+		expectedMessages = []*webrtcpb.Response{
+			{
+				Stream: &webrtcpb.Stream{Id: 1},
+				Type: &webrtcpb.Response_Headers{
+					Headers: &webrtcpb.ResponseHeaders{},
+				},
+			},
+			{
+				Stream: &webrtcpb.Stream{Id: 1},
+				Type: &webrtcpb.Response_Message{
+					Message: &webrtcpb.ResponseMessage{
+						PacketMessage: &webrtcpb.PacketMessage{
+							Data: respMd,
+							Eom:  true,
+						},
+					},
+				},
+			},
+			{
+				Stream: &webrtcpb.Stream{Id: 1},
+				Type: &webrtcpb.Response_Trailers{
+					Trailers: &webrtcpb.ResponseTrailers{
+						Status: ErrorToStatus(status.Error(codes.Canceled, "request cancelled")).Proto(),
+					},
+				},
+			},
+		}
+		messagesRead = make(chan struct{})
+		expectedMessagesMu.Unlock()
+
+		test.That(t, clientCh.writeHeaders(&webrtcpb.Stream{
+			Id: 1,
+		}, &webrtcpb.RequestHeaders{
+			Method: "/proto.rpc.webrtc.v1.SignalingService/Call",
+			Metadata: metadataToProto(metadata.MD{
+				"rpc-host": []string{"yeehaw"},
+			}),
+		}), test.ShouldBeNil)
+
+		reqMd, err := proto.Marshal(&webrtcpb.CallRequest{Sdp: "hello"})
+		test.That(t, err, test.ShouldBeNil)
+
+		test.That(t, clientCh.writeMessage(&webrtcpb.Stream{
+			Id: 1,
+		}, &webrtcpb.RequestMessage{
+			HasMessage: true,
+			PacketMessage: &webrtcpb.PacketMessage{
+				Data: reqMd,
+				Eom:  true,
+			},
+			Eos: true,
+		}), test.ShouldBeNil)
+
+		offer, err := signalServer.callQueue.RecvOffer(context.Background(), []string{"yeehaw"})
+		test.That(t, err, test.ShouldBeNil)
+		answererSDP := "world"
+		test.That(t, offer.AnswererRespond(context.Background(), WebRTCCallAnswer{InitialSDP: &answererSDP}), test.ShouldBeNil)
+		test.That(t, clientCh.writeReset(&webrtcpb.Stream{Id: 1}), test.ShouldBeNil)
+
+		<-messagesRead
+	})
+}
