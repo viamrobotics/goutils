e6ca6db60944b4d6dbd78c0b897ef02db11b5125
Eric Daniels <eric@erdaniels.com>
2022-10-24 16:30:44 -0400

    Update slice.go

--- slice.go
+++ slice.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-// RawBytesFromSlice returns a view of the given slice value. It is valida
+// RawBytesFromSlice returns a view of the given slice value. It is validab
 // as long as the given value stays within GC.
 func RawBytesFromSlice(val interface{}) []byte {
 	valV := reflect.ValueOf(val)
