18212d757f82e13e7edccf7f23447428a54acd17
Eric Daniels <eric@erdaniels.com>
2023-01-23 14:27:42 -0500

    usb: fix Search signature for not darwin/linux

--- usb/search.go
+++ usb/search.go
@@ -6,6 +6,6 @@ package usb
 type SearchFilter struct{}
 
 // Search returns nothing here for unsupported platforms.
-func Search(filter SearchFilter, includeDevice func(vendorID, productID int) bool) ([]Description, error) {
+func Search(filter SearchFilter, includeDevice func(vendorID, productID int) bool) []Description {
 	return nil, nil
 }
