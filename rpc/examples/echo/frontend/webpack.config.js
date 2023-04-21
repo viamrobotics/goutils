707274d0b673f95b4fae886bd475629015b77289
Maxim Pertsov <maxim@viam.com>
2023-04-21 15:56:33 -0400

    use local rpc package as dependency

--- rpc/examples/echo/frontend/webpack.config.js
+++ rpc/examples/echo/frontend/webpack.config.js
@@ -1,23 +1,24 @@
-var path = require('path');
+// var path = require("path");
 
 module.exports = {
-	mode: "production",
-	entry: "./src/index.ts",
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				include: /src/,
-				exclude: /node_modules/,
-				loader: "ts-loader"
-			}
-		]
-	},
-	resolve: {
-		extensions: [".ts", ".js"],
-		alias: {
-			"@viamrobotics/rpc": path.join(__dirname, "../../../js")
-		}
-	}
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: /src/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  // ,
+  // resolve: {
+  // 	extensions: [".ts", ".js"],
+  // 	alias: {
+  // 		"@viamrobotics/rpc": path.join(__dirname, "../../../js")
+  // 	}
+  // }
 };
