b5f21dcc504c4d8d346f31367e8079eedd507460
Kschappacher <56745262+Kschappacher@users.noreply.github.com>
2023-05-15 17:33:19 -0400

    limit cipher suits

--- http2.go
+++ http2.go
@@ -35,6 +35,14 @@ func NewPossiblySecureHTTPServer(handler http.Handler, opts HTTPServerOptions) (
 		if opts.TLSAuth {
 			httpServer.TLSConfig = &tls.Config{
 				MinVersion: tls.VersionTLS12,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				},
 				ClientAuth: tls.VerifyClientCertIfGiven,
 			}
 		}
