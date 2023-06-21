a27cef69a5904d65696ad4441dff5a5e49fc8ec4
Gloria Cai <gloriacai101@gmail.com>
2023-06-21 16:32:20 -0400

    add org view

--- perf/grpc.go
+++ perf/grpc.go
@@ -2,14 +2,26 @@ package perf
 
 import (
 	"go.opencensus.io/plugin/ocgrpc"
+	ocstats "go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 	"google.golang.org/grpc/stats"
 )
 
 // See: https://opencensus.io/guides/grpc/go/#1
 
 func registerGrpcViews() error {
-	return view.Register(ocgrpc.DefaultServerViews...)
+	OrgID, _ := tag.NewKey("org-id")
+	tagsView := &view.View{
+		Name:        "server/rpc_message_counts",
+		Description: "number of messages received  in each rpc call",
+		Measure: ocstats.Int64("rpc_message_counts", "Number of messages received in each RPC. Has value 1 for non-streaming RPCs.",
+			ocstats.UnitDimensionless),
+		Aggregation: view.Count(),
+		TagKeys:     []tag.Key{OrgID},
+	}
+	views := append(ocgrpc.DefaultServerViews, tagsView)
+	return view.Register(views...)
 }
 
 // NewGrpcStatsHandler creates a new stats handler writing to opencensus.
