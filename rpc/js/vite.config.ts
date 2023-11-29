e65be5d5dd17705ba2f9fa7d0aed7fbef6a67f49
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-11-29 16:28:11 -0500

    add vite config file

---
+++ rpc/js/vite.config.ts
@@ -0,0 +1,42 @@
+/// <reference types="vitest" />
+import path from 'node:path';
+import { defineConfig } from 'vite';
+
+import pkg from './package.json';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': '"production"',
+    __VERSION__: JSON.stringify(pkg.version),
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [
+        /exponential-backoff/u,
+        /google-protobuf/u,
+        /@improbable-eng\/grpc-web/u,
+        /gen\//u,
+      ],
+    },
+    minify: true,
+    target: 'esnext',
+    lib: {
+      entry: path.resolve(__dirname),
+      name: 'sdk',
+      fileName: (format) => `main.${format}.js`,
+    },
+    rollupOptions: {
+      onwarn: (warning, warn) => {
+        if (warning.code === 'EVAL') {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+  test: {
+    mockReset: true,
+  },
+});
