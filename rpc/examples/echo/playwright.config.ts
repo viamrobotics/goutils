e0dc43b822fa125718f44a5a55308b5d9e23ff35
Maxim Pertsov <maxim@viam.com>
2023-12-04 12:09:19 -0500

    remove tmp dir

--- rpc/examples/echo/playwright.config.ts
+++ rpc/examples/echo/playwright.config.ts
@@ -10,7 +10,6 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./tests",
-  outputDir: "/tmp/.playwright-test",
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
