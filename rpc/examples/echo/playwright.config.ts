6b127027ae46c2ad3e988cb18da6e0c8f5f85ccd
Maxim Pertsov <maxim@viam.com>
2023-12-04 12:10:21 -0500

    trace all failures locally

--- rpc/examples/echo/playwright.config.ts
+++ rpc/examples/echo/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     baseURL: "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: process.env.CI ? "on-first-retry" : "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
