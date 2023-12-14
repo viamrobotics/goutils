94929158aef6a26dab38df92cc452f015062d001
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-12-14 14:06:47 -0500

    fix nits

--- rpc/examples/fileupload/frontend/vite.config.ts
+++ rpc/examples/fileupload/frontend/vite.config.ts
@@ -9,14 +9,12 @@ export default defineConfig({
     __VERSION__: JSON.stringify(pkg.version),
   },
   build: {
+    // This config is necessary to transform libraries on the list into ES modules.
+    // This can be removed if protobuf-es or a code generating tool that has good
+    // support for ES modules is used.
     commonjsOptions: {
       transformMixedEsModules: true,
-      include: [
-        /exponential-backoff/u,
-        /google-protobuf/u,
-        /@improbable-eng\/grpc-web/u,
-        /gen\//u,
-      ],
+      include: [/google-protobuf/u, /@improbable-eng\/grpc-web/u, /gen\//u],
     },
     target: "esnext",
     lib: {
