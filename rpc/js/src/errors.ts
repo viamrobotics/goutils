3f02a96a66a0fddcd159d4e6dbdb4549cda80b22
DTCurrie <devintcurrie@gmail.com>
2024-01-12 15:54:30 -0500

    Errors cleanup

--- rpc/js/src/errors.ts
+++ rpc/js/src/errors.ts
@@ -1,31 +1,38 @@
+/* eslint-disable max-classes-per-file */
+
 export class ConnectionClosedError extends Error {
   constructor(msg: string) {
     super(msg);
+    this.name = 'ConnectionClosedError';
     Object.setPrototypeOf(this, ConnectionClosedError.prototype);
   }
 
-  static isError(error: any): boolean {
+  static isError(error: unknown): boolean {
     if (error instanceof ConnectionClosedError) {
       return true;
     }
+
     if (typeof error === 'string') {
       return error === 'Response closed without headers';
     }
+
     if (error instanceof Error) {
       return error.message === 'Response closed without headers';
     }
+
     return false;
   }
 }
 
 export class GRPCError extends Error {
-  public readonly code: number;
-  public readonly grpcMessage: string;
-
-  constructor(code: number, message: string) {
-    super(`Code=${code} Message=${message}`);
+  constructor(
+    readonly code: number,
+    readonly grpcMessage: string
+  ) {
+    super(`Code=${code} Message=${grpcMessage}`);
+    this.name = 'GRPCError';
     this.code = code;
-    this.grpcMessage = message;
+    this.grpcMessage = grpcMessage;
     Object.setPrototypeOf(this, GRPCError.prototype);
   }
 }
