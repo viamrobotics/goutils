2305b7d48393519a0f6a89a4231a38b6552a5b83
Eric Daniels <eric@erdaniels.com>
2024-03-21 09:39:26 -0400

    fix key gen in examples

--- etc/setup_priv_key.sh
+++ etc/setup_priv_key.sh
@@ -5,6 +5,6 @@ cd test_keys
 if [[ -f "private-key.pem" && -f "pkcs8.key" ]]; then
 	exit 0;
 fi
-openssl genrsa -out private-key.pem 4096 
+openssl genpkey -algorithm ed25519 -out private-key.pem
 openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in private-key.pem -out pkcs8.key
-openssl rsa -in private-key.pem -outform PEM -pubout -out public-key.pem
+openssl pkey -in private-key.pem -pubout -out public-key.pem
