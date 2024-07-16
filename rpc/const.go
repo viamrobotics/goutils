e79930c763c8458babfcd301889de01e4fdaed73
Cheuk Tse <cheuk@viam.com>
2024-07-16 12:26:39 -0400

    some improvements

--- rpc/const.go
+++ rpc/const.go
@@ -1,4 +1,11 @@
 package rpc
 
-// MaxMessageSize is the maximum size a gRPC message can be.
-var MaxMessageSize = 1 << 25
+import "time"
+
+var (
+	// MaxMessageSize is the maximum size a gRPC message can be.
+	MaxMessageSize = 1 << 25
+
+	// keepAliveTime is how often to establish Keepalive pings/expectations.
+	keepAliveTime = 10 * time.Second
+)
