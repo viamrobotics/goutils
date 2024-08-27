776794317db253ec2446581ac877a7582d7f7b2b
Benji Rewis <benji.rewis@viam.com>
2024-08-27 11:25:58 -0400

    update tests

--- stoppable_workers_test.go
+++ stoppable_workers_test.go
@@ -1,11 +1,10 @@
 package utils_test
 
 import (
+	"bytes"
 	"context"
 	"testing"
 	"time"
-
-	"go.viam.com/test"
 
 	"go.viam.com/utils"
 )
@@ -17,7 +16,23 @@ func TestStoppableWorkers(t *testing.T) {
 
 	t.Run("one worker", func(t *testing.T) {
 		sw := utils.NewStoppableWorkers(ctx)
-		test.That(t, sw.Add(normalWorker), test.ShouldBeNil)
+		sw.Add(normalWorker)
+		sw.Stop()
+	})
+
+	t.Run("one worker background constructor", func(t *testing.T) {
+		sw := utils.NewBackgroundStoppableWorkers(normalWorker)
+		sw.Stop()
+	})
+
+	t.Run("heavy workers", func(t *testing.T) {
+		sw := utils.NewStoppableWorkers(ctx)
+		sw.Add(heavyWorker)
+		sw.Add(heavyWorker)
+		sw.Add(heavyWorker)
+
+		// Sleep for a second to let heavy workers do work.
+		time.Sleep(1 * time.Second)
 		sw.Stop()
 	})
 
@@ -26,30 +41,28 @@ func TestStoppableWorkers(t *testing.T) {
 		concurrentWorker := func(ctx context.Context) {
 			go normalWorker(ctx)
 		}
-		test.That(t, sw.Add(concurrentWorker), test.ShouldBeNil)
-		test.That(t, sw.Add(concurrentWorker), test.ShouldBeNil)
+		sw.Add(concurrentWorker)
 		sw.Stop()
 	})
 
 	t.Run("nested workers", func(t *testing.T) {
 		sw := utils.NewStoppableWorkers(ctx)
-		test.That(t, sw.Add(nestedWorkersWorker), test.ShouldBeNil)
+		sw.Add(nestedWorkersWorker)
 		sw.Stop()
 	})
 
 	t.Run("panicking worker", func(t *testing.T) {
 		sw := utils.NewStoppableWorkers(ctx)
 		// Both adding and stopping a panicking worker should cause no `panic`s.
-		test.That(t, sw.Add(panickingWorker), test.ShouldBeNil)
+		sw.Add(panickingWorker)
 		sw.Stop()
 	})
 
 	t.Run("already stopped", func(t *testing.T) {
 		sw := utils.NewStoppableWorkers(ctx)
 		sw.Stop()
-		test.That(t, sw.Add(normalWorker), test.ShouldBeError,
-			utils.ErrStoppableWorkersAlreadyStopped)
-		sw.Stop() // stopping twice should cause no `panic`
+		sw.Add(normalWorker) // adding after Stop should cause no `panic`
+		sw.Stop()            // stopping twice should cause no `panic`
 	})
 }
 
@@ -59,6 +72,22 @@ func normalWorker(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case <-time.After(10 * time.Millisecond):
+		}
+	}
+}
+
+// like `normalWorker`, but writes and reads bytes from a buffer.
+func heavyWorker(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(10 * time.Millisecond):
+			var buffer bytes.Buffer
+			data := make([]byte, 10000)
+			buffer.Write(data)
+			readData := make([]byte, buffer.Len())
+			buffer.Read(readData)
 		}
 	}
 }
