012b98fe7ada25015c800a34545dec1b6828cd91
Benji Rewis <benji.rewis@viam.com>
2024-08-27 16:55:03 -0400

    better concurrent test

--- stoppable_workers_test.go
+++ stoppable_workers_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 	"time"
 
+	"go.viam.com/test"
+
 	"go.viam.com/utils"
 )
 
@@ -18,11 +20,17 @@ func TestStoppableWorkers(t *testing.T) {
 		sw := utils.NewStoppableWorkers(ctx)
 		sw.Add(normalWorker)
 		sw.Stop()
+		ctx := sw.Context()
+		test.That(t, ctx, test.ShouldNotBeNil)
+		test.That(t, ctx.Err(), test.ShouldBeError, context.Canceled)
 	})
 
 	t.Run("one worker background constructor", func(t *testing.T) {
 		sw := utils.NewBackgroundStoppableWorkers(normalWorker)
 		sw.Stop()
+		ctx := sw.Context()
+		test.That(t, ctx, test.ShouldNotBeNil)
+		test.That(t, ctx.Err(), test.ShouldBeError, context.Canceled)
 	})
 
 	t.Run("heavy workers", func(t *testing.T) {
@@ -31,18 +39,42 @@ func TestStoppableWorkers(t *testing.T) {
 		sw.Add(heavyWorker)
 		sw.Add(heavyWorker)
 
-		// Sleep for a second to let heavy workers do work.
-		time.Sleep(1 * time.Second)
+		// Sleep for half a second to let heavy workers do work.
+		time.Sleep(500 * time.Millisecond)
 		sw.Stop()
 	})
 
 	t.Run("concurrent workers", func(t *testing.T) {
-		sw := utils.NewStoppableWorkers(ctx)
-		concurrentWorker := func(ctx context.Context) {
-			go normalWorker(ctx)
+		ints := make(chan int)
+		writeWorker := func(ctx context.Context) {
+			var count int
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(100 * time.Millisecond):
+					ints <- count
+				}
+			}
 		}
-		sw.Add(concurrentWorker)
+		var receivedInts []int
+		readWorker := func(ctx context.Context) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case i := <-ints:
+					receivedInts = append(receivedInts, i)
+				}
+			}
+		}
+
+		sw := utils.NewBackgroundStoppableWorkers(writeWorker, readWorker)
+		// Sleep for a second to let concurrent workers do work.
+		time.Sleep(500 * time.Millisecond)
 		sw.Stop()
+
+		test.That(t, len(receivedInts), test.ShouldBeGreaterThan, 0)
 	})
 
 	t.Run("nested workers", func(t *testing.T) {
