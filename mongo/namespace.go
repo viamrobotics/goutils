cbc56492d2b3a036f946443859b7830ca490ebdb
Steven Balasta <sbal13@gmail.com>
2024-09-11 14:32:11 -0400

    [APP-5672] lowercase and lengthen random string to pmitigate name collision

--- mongo/namespace.go
+++ mongo/namespace.go
@@ -3,6 +3,7 @@ package mongoutils
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 
 	"go.viam.com/utils"
@@ -100,10 +101,10 @@ func RandomizeNamespaces() (newNamespaces map[string][]string, restore func()) {
 	defer namespacesMu.Unlock()
 	oldNamespaces := map[randomizedName][]randomizedName{}
 	for db, colls := range namespaces {
-		newDBName := randomizedName{ptr: db, from: *db, to: "test-" + utils.RandomAlphaString(5)}
+		newDBName := randomizedName{ptr: db, from: *db, to: "test-" + strings.ToLower(utils.RandomAlphaString(20))}
 		oldNamespaces[newDBName] = nil
 		for _, coll := range colls {
-			newCollName := randomizedName{ptr: coll, from: *coll, to: utils.RandomAlphaString(5)}
+			newCollName := randomizedName{ptr: coll, from: *coll, to: strings.ToLower(utils.RandomAlphaString(20))}
 			oldNamespaces[newDBName] = append(oldNamespaces[newDBName], newCollName)
 			*coll = newCollName.to
 		}
