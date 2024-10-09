90218f0fe62622f4b7c55d4d20f6ac6717053e5c
DTCurrie <devintcurrie@gmail.com>
2024-10-09 13:50:22 -0400

    test cleanup

--- web/auth_test.go
+++ web/auth_test.go
@@ -7,152 +7,63 @@ import (
 	"go.viam.com/test"
 )
 
+func createRequest(t *testing.T) *http.Request {
+	r, err := http.NewRequest(http.MethodGet, "http://localhost/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return r
+}
+
+func setCookie(t *testing.T, r *http.Request, key string, value string) {
+	r.AddCookie(&http.Cookie{
+		Name:     key,
+		Value:    value,
+		Path:     "/",
+		MaxAge:   10000,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+		HttpOnly: true,
+	})
+}
+
 func TestWebAuth(t *testing.T) {
+
 	t.Run("should return nil when token cookie is not set", func(t *testing.T) {
-		r, err := http.NewRequest(http.MethodGet, "http://localhost/", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthToken,
-			Value:    "",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthRefresh,
-			Value:    "",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthExpiry,
-			Value:    "123456",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
+		r := createRequest(t)
+		setCookie(t, r, ViamAuthToken, "")
+		setCookie(t, r, ViamAuthRefresh, "")
+		setCookie(t, r, ViamAuthExpiry, "123456")
 
 		data := getAuthCookieValues(r)
 		test.That(t, data, test.ShouldBeNil)
 	})
 
 	t.Run("should return nil when refresh cookies causes an error", func(t *testing.T) {
-		r, err := http.NewRequest(http.MethodGet, "http://localhost/", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthToken,
-			Value:    "abc123",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthExpiry,
-			Value:    "123456",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
+		r := createRequest(t)
+		setCookie(t, r, ViamAuthToken, "abc123")
+		setCookie(t, r, ViamAuthExpiry, "123456")
 
 		data := getAuthCookieValues(r)
 		test.That(t, data, test.ShouldBeNil)
 	})
 
 	t.Run("should return nil when expiry cookie is not set", func(t *testing.T) {
-		r, err := http.NewRequest(http.MethodGet, "http://localhost/", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthToken,
-			Value:    "abc123",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthRefresh,
-			Value:    "",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthExpiry,
-			Value:    "",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
+		r := createRequest(t)
+		setCookie(t, r, ViamAuthToken, "abc123")
+		setCookie(t, r, ViamAuthRefresh, "")
+		setCookie(t, r, ViamAuthExpiry, "")
 
 		data := getAuthCookieValues(r)
 		test.That(t, data, test.ShouldBeNil)
 	})
 
 	t.Run("should return token response data when cookies are set", func(t *testing.T) {
-		r, err := http.NewRequest(http.MethodGet, "http://localhost/", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthToken,
-			Value:    "abc123",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthRefresh,
-			Value:    "",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
-
-		r.AddCookie(&http.Cookie{
-			Name:     ViamAuthExpiry,
-			Value:    "123456",
-			Path:     "/",
-			MaxAge:   10000,
-			Secure:   true,
-			SameSite: http.SameSiteLaxMode,
-			HttpOnly: true,
-		})
+		r := createRequest(t)
+		setCookie(t, r, ViamAuthToken, "abc123")
+		setCookie(t, r, ViamAuthRefresh, "")
+		setCookie(t, r, ViamAuthExpiry, "123456")
 
 		data := getAuthCookieValues(r)
 		test.That(t, data.AccessToken, test.ShouldEqual, "abc123")
