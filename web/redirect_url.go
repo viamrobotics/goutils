09c30e5aad8f860fc30a2fe23b4bb68db298e299
DTCurrie <devintcurrie@gmail.com>
2024-11-12 10:56:04 -0500

    add comment

--- web/redirect_url.go
+++ web/redirect_url.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+// IsLocalRedirectPath returns true if the passed string is a valid local pathname for the local server:
+//   - https://example.com -> false
+//   - /local/path/name -> true
 func IsLocalRedirectPath(path string) bool {
 	normalized := strings.ReplaceAll(path, "\\", "/")
 	url, err := url.Parse(normalized)
