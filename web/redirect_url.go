afc39f2d887b192e9ee47d2663cd582aed3f9896
DTCurrie <devintcurrie@gmail.com>
2024-11-14 09:34:37 -0500

    whitelist domains and restrict to https or local paths

--- web/redirect_url.go
+++ web/redirect_url.go
@@ -5,20 +5,42 @@ import (
 	"strings"
 )
 
-// IsLocalRedirectPath returns true if the passed string is a valid local pathname for the local server:
+var hostname_whitelist = map[string]bool{
+	"localhost": true,
+	"viam.dev":  true,
+	"viam.com":  true,
+}
+
+func isWhitelisted(hostname string) bool {
+	return hostname_whitelist[hostname]
+}
+
+// IsLocalRedirectPath returns true if the passed string is a secure URL to a whitelisted hostname or a valid local pathname for the local
+// server. The whitelisted hostnames are: "localhost", "viam.dev", and "viam.com".
+//
 //   - https://example.com -> false
+//   - http://viam.com/path/name -> false
+//   - https://viam.com/path/name -> true
 //   - /local/path/name -> true
 func IsLocalRedirectPath(path string) bool {
 	normalized := strings.ReplaceAll(path, "\\", "/")
-	url, err := url.Parse(normalized)
+	url, err := url.ParseRequestURI(normalized)
 	if err != nil {
+		// ignore invalid URLs/URL components
 		return false
 	}
 
-	if url.IsAbs() {
+	if url.Scheme != "" && url.Scheme != "https" {
+		// ignore non-secure URLs
 		return false
 	}
 
+	if isWhitelisted(url.Hostname()) {
+		// ignore non-whitelisted hosts
+		return true
+	}
+
+	// allow local app paths
 	candidate := url.String()
 	return strings.HasPrefix(candidate, "/") && !strings.HasPrefix(candidate, "//")
 }
--- web/redirect_url_test.go
+++ web/redirect_url_test.go
@@ -7,14 +7,64 @@ import (
 )
 
 func TestIsLocalRedirectPath(t *testing.T) {
-	t.Run("check valid redirect paths", func(t *testing.T) {
+	t.Run("rejects external URLs", func(t *testing.T) {
 		test.That(t, IsLocalRedirectPath("https://example.com"), test.ShouldBeFalse)
 		test.That(t, IsLocalRedirectPath("http://example.com"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("ftp://example.com"), test.ShouldBeFalse)
 		test.That(t, IsLocalRedirectPath("://example.com"), test.ShouldBeFalse)
 		test.That(t, IsLocalRedirectPath("//example.com"), test.ShouldBeFalse)
 		test.That(t, IsLocalRedirectPath("example.com"), test.ShouldBeFalse)
-		test.That(t, IsLocalRedirectPath("ftp://example.com"), test.ShouldBeFalse)
 		test.That(t, IsLocalRedirectPath("www.example.com"), test.ShouldBeFalse)
+	})
+
+	t.Run("rejects invalid production URLs", func(t *testing.T) {
+		test.That(t, IsLocalRedirectPath("http://viam.com"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("ftp://viam.com"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("://viam.com"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("//viam.com"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("//viam.com/some/path"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("viam.com"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("viam.com/some/path"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("www.viam.com"), test.ShouldBeFalse)
+	})
+
+	t.Run("accepts valid production URLs", func(t *testing.T) {
+		test.That(t, IsLocalRedirectPath("https://viam.com"), test.ShouldBeTrue)
+		test.That(t, IsLocalRedirectPath("https://viam.com/some/path"), test.ShouldBeTrue)
+	})
+
+	t.Run("rejects invalid staging URLs", func(t *testing.T) {
+		test.That(t, IsLocalRedirectPath("http://viam.dev"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("ftp://viam.dev"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("://viam.dev"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("//viam.dev"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("//viam.dev/some/path"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("viam.dev"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("viam.dev/some/path"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("www.viam.dev"), test.ShouldBeFalse)
+	})
+
+	t.Run("accepts valid staging URLs", func(t *testing.T) {
+		test.That(t, IsLocalRedirectPath("https://viam.dev"), test.ShouldBeTrue)
+		test.That(t, IsLocalRedirectPath("https://viam.dev/some/path"), test.ShouldBeTrue)
+	})
+
+	t.Run("rejects invalid local URLs", func(t *testing.T) {
+		test.That(t, IsLocalRedirectPath("http://localhost"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("ftp://localhost"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("://localhost"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("//localhost"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("//localhost/some/path"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("localhost"), test.ShouldBeFalse)
+		test.That(t, IsLocalRedirectPath("localhost/some/path"), test.ShouldBeFalse)
+	})
+
+	t.Run("accepts valid local URLs", func(t *testing.T) {
+		test.That(t, IsLocalRedirectPath("https://localhost"), test.ShouldBeTrue)
+		test.That(t, IsLocalRedirectPath("https://localhost/some/path"), test.ShouldBeTrue)
+	})
+
+	t.Run("accepts path names", func(t *testing.T) {
 		test.That(t, IsLocalRedirectPath("/local/path?myparam=test"), test.ShouldBeTrue)
 	})
 }
