7c0f9dbd610d425427120fd1697af8a779c968c7
Bashar Eid <bashar.eid@viam.com>
2025-01-14 12:20:11 -0500

    remove ineffectual assignment

--- logger.go
+++ logger.go
@@ -146,7 +146,6 @@ func LogFinalLine(logger ZapCompatibleLogger, startTime time.Time, err error, ms
 		if level < zap.ErrorLevel {
 			level = zap.ErrorLevel
 		}
-		level = zap.ErrorLevel
 		fields = append(fields, "error", err)
 	}
 	fields = append(fields, "grpc.code", code.String(), "grpc.time_ms", duration)
