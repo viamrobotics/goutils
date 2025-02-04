e55a28f542202825e273be9c389424fae52a8fc5
Bashar Eid <bashar.eid@viam.com>
2025-02-04 18:07:13 -0500

    lint again again again

--- rpc/dial_options.go
+++ rpc/dial_options.go
@@ -284,7 +284,7 @@ func WithForceDirectGRPC() DialOption {
 	})
 }
 
-// WithExternalSignalingConn provides a preexisting connection to use. This option forces the webrtcSignalingAnswerer to not dial or manage
+// WithSignalingConn provides a preexisting connection to use. This option forces the webrtcSignalingAnswerer to not dial or manage
 // a connection.
 func WithSignalingConn(signalingConn ClientConn) DialOption {
 	return newFuncDialOption(func(o *dialOptions) {
