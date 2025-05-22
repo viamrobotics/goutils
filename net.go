3200427a7540d3478e58fd28ad511bc21626e335
Devin Hilly <devin.hilly@viam.com>
2025-05-22 12:01:51 -0400

    DATA-4118 - Add ReserveRandomPort (#434)

--- net.go
+++ net.go
@@ -26,6 +26,16 @@ func TryReserveRandomPort() (port int, err error) {
 	return listener.Addr().(*net.TCPAddr).Port, nil
 }
 
+// ReserveRandomPort reserves a random port and returns the port number and the listener.
+func ReserveRandomPort() (int, net.Listener, error) {
+	//nolint:gosec
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return 0, nil, err
+	}
+	return listener.Addr().(*net.TCPAddr).Port, listener, nil
+}
+
 // GetAllLocalIPv4s finds all the local ips from all interfaces
 // It only returns IPv4 addresses, and tries not to return any loopback addresses.
 func GetAllLocalIPv4s() ([]string, error) {
