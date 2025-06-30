9004d20e02eadfb2a2292e6a3e7f8b0e85000bec
Joshua Matthews <josh.matthews@viam.com>
2025-06-30 10:22:14 -0400

    Document TURN env vars as experimental

--- rpc/const.go
+++ rpc/const.go
@@ -19,20 +19,24 @@ var (
 	OnlySocksProxyEnvVar = "ONLY_SOCKS_PROXY"
 
 	// TURNURIEnvVar is the name of an environment variable used to select at
-	// most one TURN server.
+	// most one TURN server. This parameter is experimental and may be changed or
+	// removed in future versions.
 	TURNURIEnvVar = "TURN_URI"
 
 	// TURNPortEnvVar is the name of an environment variable used to override the
-	// port used for TURN if a TURN server is configured.
+	// port used for TURN if a TURN server is configured. This parameter is
+	// experimental and may be changed or removed in future versions.
 	TURNPortEnvVar = "TURN_PORT"
 
 	// TURNSchemeEnvVar is the name of an environment variable used to override
 	// the scheme used for TURN if a TURN server is configured. Must be either
-	// turn or turns.
+	// turn or turns. This parameter is experimental and may be changed or
+	// removed in future versions.
 	TURNSchemeEnvVar = "TURN_SCHEME"
 
 	// TURNTransportEnvVar is the name of an environment variable used to
 	// override the transport used for TURN if a TURN server is configured. Must
-	// be either tcp or udp.
+	// be either tcp or udp. This parameter is experimental and may be changed or
+	// removed in future versions.
 	TURNTransportEnvVar = "TURN_TRANSPORT"
 )
