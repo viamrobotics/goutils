b1cfab3050b65bc691e05153dbe225791bf8dd08
Joshua Matthews <josh.matthews@viam.com>
2025-11-14 15:12:22 -0500

    Fix lints

--- trace/trace.go
+++ trace/trace.go
@@ -16,12 +16,17 @@ var (
 	tracer        trace.Tracer         = traceProvider.Tracer("unconfigured")
 )
 
+// Span is a type alias to [trace.Span].
 type Span = trace.Span
 
+// GetProvider returns the [trace.TracerProvider] configured by the last call to
+// [SetTracerWithExporters].
 func GetProvider() trace.TracerProvider {
 	return traceProvider
 }
 
+// SetTracerWithExporters creates a [trace.TracerProvider] and stores it for
+// global use.
 func SetTracerWithExporters(resource *otelresource.Resource, exporters ...sdktrace.SpanExporter) {
 	opts := make([]sdktrace.TracerProviderOption, 0, 1+len(exporters))
 	opts = append(opts, sdktrace.WithResource(resource))
@@ -32,6 +37,8 @@ func SetTracerWithExporters(resource *otelresource.Resource, exporters ...sdktra
 	tracer = traceProvider.Tracer("go.viam.com/rdk")
 }
 
+// Shutdown shuts down the global [trace.TracerProvider] created with
+// [SetTracerWithExporters].
 func Shutdown(ctx context.Context) error {
 	if sdkTraceProvider, ok := traceProvider.(*sdktrace.TracerProvider); ok {
 		return sdkTraceProvider.Shutdown(ctx)
