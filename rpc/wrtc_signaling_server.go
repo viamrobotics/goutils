1cc19c39e48eed92b71f553cc0b62d05c335fc1b
Benjamin Rewis <benji.rewis@viam.com>
2026-09-03 10:53:37 -0400

    Address Daniel feedback

--- rpc/wrtc_signaling_server.go
+++ rpc/wrtc_signaling_server.go
@@ -201,15 +201,6 @@ func (srv *WebRTCSignalingServer) Call(req *webrtcpb.CallRequest, server webrtcp
 	var callerAuthMetadata map[string]string
 	if entity, ok := ContextAuthEntity(ctx); ok {
 		callerAuthEntity, callerAuthMetadata = entity.Entity, entity.AuthMetadata
-
-		// Auth metadata (app user ID, email) should be in the JWT token, but the
-		// internalForeverJWT used by the machine page has no metadata. Pull it off
-		// `entity.Data`.
-		if callerAuthMetadata == nil {
-			if provider, ok := entity.Data.(interface{ CreateMetadata() map[string]string }); ok {
-				callerAuthMetadata = provider.CreateMetadata()
-			}
-		}
 	}
 	uuid, respCh, respDone, sendCancel, err := srv.callQueue.SendOfferInit(
 		ctx, host, req.GetSdp(), req.GetDisableTrickle(), callerAuthEntity, callerAuthMetadata)
